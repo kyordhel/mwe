c044d3b6ad6eb7f3b87a11591098d67d0fc827a9
Mauricio Matamoros <kyordhel@gmail.com>
2024-02-05 21:49:23 -0600

    Add mwe: Hide python comments in latex with listings

---
+++ listings-py/mwe.py
@@ -0,0 +1,11 @@
+from time import sleep
+
+def foo():
+    if cond1: bar() #Some explanation
+    if cond2: baz() #Some other explanation
+    if cond3: bye() #Even more explanations
+
+    # Another line that does nothing
+    while True:
+        sleep(1)
+#end def
