970201c1d829f6c7aae74871318d30683387553e
Mauricio Matamoros <kyordhel@gmail.com>
2024-02-11 15:16:51 -0600

    Add nickie's solution and update mwe.py

--- listings-py/mwe.py
+++ listings-py/mwe.py
@@ -1,18 +1,29 @@
 from time import sleep
 
 def foo():
-    if cond1: bar() #Some explanation
-    if cond2: baz() #Some other explanation
-    if cond3: bye() #Even more explanations
+	if cond1: bar() #Some explanation
+	if cond2: baz() #Some other explanation
+	if cond3: bye() #Even more explanations
 
-    # Another line that does nothing
-    while True:
-        sleep(1)
-    # The following three lines explain
-    # what the next for does. Which is
-    #basically nothing because it passes
-    for i in range(100):
-        # Another line that should not print
-        pass # And an unnecessary explanation
+	# Another line that does nothing
+	while True:
+		sleep(1)
+	# The following three lines explain
+	# what the next for does. Which is
+	#basically nothing because it passes
+	for i in range(100):
+		# Another line that should not print
+		pass # And an unnecessary explanation
+
+	# Do nothing
+	pass
+	pass
+
+	# Another commend
+	# Two lines
+	pass
+	# Yet another comment
+	pass
+	pass
 
 #end def
